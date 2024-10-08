d90a6ecf2e8d99dda39ef9d8586845e22b0232aa
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2024-10-07 22:53:35 -0400

    assets folder

--- src/App.js
+++ src/App.js
@@ -2,32 +2,30 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import React from "react";
-// import About from "./About";
+import About from './components/About';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+
 
 
 
 function App() {
   return (
-    <div>
-
-
-    <Router>
-      <NavBar />
-    <Routes>
-      <Route path ="/" element = {<Home/>}/>
-   
-    </Routes>
-
-
-    </Router>
-
-
-
-
-
-
+    <div className='flex flex-col min-h-screen'>
+      <Router>
+        <NavBar />
+        <main className='flex-grow'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </main>
+      </Router>
+      <Footer />
     </div>
-
   );
 }
 
