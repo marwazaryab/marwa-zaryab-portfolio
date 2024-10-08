ff0a6c848a59445f62e3f2992ab84a2a7cce81ee
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2024-10-07 22:51:54 -0400

    fix nav bar on mobile version

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import { Link } from 'react-router-dom';
 
+
 const NavBar = () => {
 
-    const [nav, setNav] = useState(false);
+    const [nav, setNav] = useState(false); // State for when navigation bar is on
 
+    // links to all pages
     const links = [
         {
             id: 1,
@@ -14,19 +16,24 @@ const NavBar = () => {
         },
         {
             id: 2,
-            link: 'About'
+            link: 'About',
+            path: '/about'
         },
         {
             id: 3,
-            link: 'Projects'
+            link: 'Projects',
+            path: '/projects'
         },
         {
             id: 4,
-            link: 'Contact'
+            link: 'Contact',
+            path: 'contact'
         },
 
     ]
     return (
+
+        // main bracket
         <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-green-950 fixed'>
             <div>
                 <h1 className="text-5xl font-bold ml-2"> MZ</h1>
@@ -34,22 +41,22 @@ const NavBar = () => {
 
             <ul className="hidden md:flex">
 
-                {links.map(({ id, link, path }) => (
+
+                {links.map(({ id, link, path }) => ( //map all links, set to enlarge when cursor is hovered
                     <li key={id}
                         className="px-4 cursor-pointer captialize font-medium
                      text-white hover:scale-105 duration-200">
-                        <Link to ={path}>
-                        {link}
-                        
+                        <Link to={path}>
+                            {link}
+
                         </Link>
                     </li>
                 ))}
+
+
             </ul>
 
-
-            <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 text-black
-            md:hidden">
-
+            <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 text-black md:hidden">
                 {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
             </div>
 
@@ -59,21 +66,17 @@ const NavBar = () => {
                     {links.map(({ id, link, path }) => (
                         <li key={id}
                             className="px-4 cursor-pointer capitalize py-6 text-4xl text-black">
-                            <Link to ={path}>
-                        {link}
-                        
-                        </Link>
+                            <Link to={path} onClick={() => setNav(false)}>
+                                {link}
+
+                            </Link>
                         </li>
                     ))}
 
                 </ul>
-
-
             )
 
             }
-
-
 
         </div>
     )
