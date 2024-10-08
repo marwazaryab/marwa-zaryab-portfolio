72ead229c368903963d559e03a1130df041fdfd5
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2024-10-07 22:52:14 -0400

    add functionality for navigating to projects through a button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import marwa from '../assets/marwa.jpg';
 import { FaArrowRight } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa";
-import { FaSquareGithub } from "react-icons/fa6";
-import { MdEmail } from "react-icons/md";
+import { useNavigate } from 'react-router-dom';
+
 
 
 
 const Home = () => {
+
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/projects');
+
+  }
+
   return (
-    <div 
-      name="home" 
+    <div
       className='h-screen w-full bg-gradient-to-b from-white via-white to-green-100 text-black'
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className='flex flex-col justify-center h-full md:w-1/2'>
+
 
           <h1 className='text-2xl'>
             hey, i'm
@@ -28,48 +34,28 @@ const Home = () => {
             a Systems Design Engineering Student at University of Waterloo
           </p>
           <div>
-            <button className='group flex items-center bg-green-950 text-white px-6 py-3 my-2 w-fit 
-            rounded-md cursor-pointer'>My projects
+
+            <button onClick={handleClick} className='group flex items-center bg-green-950 text-white px-6 py-3 my-2 w-fit 
+            rounded-md cursor-pointer'>My Projects
               <span className='group-hover:rotate-90 duration-300'>
 
-              <FaArrowRight />
+                <FaArrowRight />
               </span>
-              
+
             </button>
           </div>
         </div>
         <div className='mt-2 md:mt-0 md:w-1/2 flex justify-center items-center'>
-          <img 
-            src={marwa} 
-            alt="Marwa" 
-            className='rounded-2xl mx-auto w-1/2 md:w-full' 
+          <img
+            src={marwa}
+            alt="Marwa"
+            className='rounded-2xl mx-auto w-1/2 md:w-full'
 
           />
         </div>
       </div>
 
-      <footer className=" bg-green-950 text-white text-center py-4">
-      <p>© 2024 Marwa Zaryab</p>
-      <div className='justify-center flex col'>
-        <a href = "https://www.linkedin.com/in/marwazaryab/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-        <FaLinkedin size={28}/>
-        </a>
-        <a href = "https://github.com/marwazaryab" target="_blank" rel="noopener noreferrer" aria-label="Github">
-        <FaSquareGithub size={28}/>
 
-        </a>
-
-        <a href = "mailto:mzzaryab@uwaterloo.ca.ca" target="_blank" aria-label="Email">
-
-        <MdEmail size={28}/>
-
-        </a>
-
-      </div>
-
-
-
-    </footer>
 
     </div>
   );
